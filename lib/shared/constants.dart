dd09d9175dd6b0558448ef14fe012d27a649de22
Ritesh Kumar <riteshkumar@Riteshs-MacBook-Air.local>
2023-06-23 04:19:17 +0530

    initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Constants{
+  static String appId="1:1067933552467:web:f638ac43d728c019d05ba6";
+  static String apiKey="AIzaSyD-Nmngs4MdvK9WJMi2hi2QBMXWQtVH7J8";
+  static String messagingSenderId="1067933552467";
+  static String projectId="chatappflutter-c1665";
+  final primaryColor= const Color(0xFFee7b64);
+  }
